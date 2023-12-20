6ebae6eb462c309395f8874888b8378b03f405dd
Şahin Akkaya <sahin@sahinakkaya.dev>
2023-12-20 17:37:31 +0300

    use lazy.nvim instead of packer.nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.g.transparent_enabled = false
+local fn = vim.fn
 
 local options = {
   backup = true,                          -- creates a backup file
@@ -261,55 +262,39 @@ M.define_augroups(M.autocmds)
 
 
 
-local fn = vim.fn
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-
-
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+require("lazy").setup({
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+ 	{
+ 		"christoomey/vim-tmux-navigator",
+    keys = {"<C-l>", "<C-h>", "<C-j>", "<C-k>"},
+ 		config = function()
+ 			vim.g.tmux_navigator_disable_when_zoomed = 1
+ 			vim.g.tmux_navigator_no_wrap = 1
+ 		end,
+ 	},
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
 })
-
-
--- Install your plugins here
-return packer.startup(function(use)
-	-- improve startup time
-
-	use({
-		"christoomey/vim-tmux-navigator",
-		config = function()
-			vim.g.tmux_navigator_disable_when_zoomed = 1
-			vim.g.tmux_navigator_no_wrap = 1
-		end,
-	})
-
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
-
-
