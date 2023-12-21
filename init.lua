5231587d06012d79540f8b4efafe04b63559c3ba
Şahin Akkaya <sahin@sahinakkaya.dev>
2023-12-21 21:48:32 +0300

    Load comment plugin on visual mode too

--- init.lua
+++ init.lua
@@ -295,7 +295,10 @@ require("lazy").setup({
   "JoosepAlviste/nvim-ts-context-commentstring",
  	{
     "numToStr/Comment.nvim",
-    keys = {"gc", "gb"},
+    keys = {
+      {"gc", mode={"n", "v"}}, 
+      {"gb", mode={"n", "v"}}, 
+    },
     config = function()
       require("Comment").setup({
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
